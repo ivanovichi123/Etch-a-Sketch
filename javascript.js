f38657b04477ed6204d70d36d25a7246009c95fe
ivanovichi <ivan.arthur18@gmail.com>
2024-06-18 16:15:40 -0600

    Finish

--- javascript.js
+++ javascript.js
@@ -1,19 +1,35 @@
 const grid = document.querySelector("#grid");
-function squares(){
-    for (i = 0; i < 256; i++){
+let amount = 16
+let size = 640000/(amount * amount);
+let realSize = Math.sqrt(size); //This operation with the previous one is used to resize correctly the size of every square
+const change = document.querySelector("#change");
+
+change.addEventListener ("click", () => {
+    amount = prompt("Select the size of the grid", 16);
+    if (amount > 100) {
+        alert("Enter a value between 0 - 100 please.");
+    } else {
+        size = 640000/(amount * amount);
+        realSize = Math.sqrt(size);
+        const remove = document.querySelectorAll(".square");
+        remove.forEach((grid) => grid.remove());
+        squares(amount);
+    }
+ })
+
+function squares(amount){
+    for (i = 0; i < amount * amount; i++){
         const square = document.createElement("div");
+        square.classList.add("square");
         square.style.boxSizing = "border-box";
-        square.style.height = "50px";
-        square.style.width = "50px";
+        square.style.height = realSize + "px";
+        square.style.width = realSize + "px";
         square.style.backgroundColor = "white";
-        // square.style.borderStyle = "solid";
-        // square.style.borderColor = "black";
         grid.appendChild(square);
         square.addEventListener ("mousemove", () => {
             square.style.backgroundColor = "black";
         })
     }
-}    
-squares();
+}  
 
-
+squares(amount);
